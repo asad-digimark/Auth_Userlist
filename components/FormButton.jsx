cec3adbd575aa72e20d348479400223e9a111a4d
Asad.Digimark <asad.digimark@gmail.com>
2023-11-03 10:51:01 +0500

    fontawesome icons

--- components/FormButton.jsx
+++ components/FormButton.jsx
@@ -1,14 +1,18 @@
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 import {Dimensions} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const height = Dimensions.get('window').height / 16;
 
-export default ({title, backgroundColor, color, ...rest}) => (
+export default ({title, backgroundColor, icon, color, ...rest}) => (
   <TouchableOpacity
     style={[
       styles.btnContainer,
       {backgroundColor, opacity: rest.disabled ? 0.7 : 1},
     ]}
     {...rest}>
+    {icon && (
+      <FontAwesome name={icon} size={24} style={{alignSelf: 'center'}} />
+    )}
     <Text style={[styles.btnText, {color}]}>{title}</Text>
   </TouchableOpacity>
 );
@@ -16,10 +20,14 @@ export default ({title, backgroundColor, color, ...rest}) => (
 const styles = StyleSheet.create({
   btnContainer: {
     height,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     borderRadius: 8,
+    paddingHorizontal: 12,
   },
   btnText: {
+    flex: 1,
     fontSize: 18,
     fontWeight: '500',
     textAlign: 'center',
