74fa41c4e8df0f3f9af980019cd4a2c6c79f3b27
Asad.Digimark <asad.digimark@gmail.com>
2023-11-03 11:10:19 +0500

    fontawesome and logout btn

--- navigation/AppStack.jsx
+++ navigation/AppStack.jsx
@@ -1,26 +1,19 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import UserList from '../screens/UserList';
-import FormButton from '../components/FormButton';
 import {useAuthContext} from '../auth/AuthProvider';
+import {Button} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default () => {
   const {logout} = useAuthContext();
   return (
-    <Stack.Navigator initialRouteName="Users done">
+    <Stack.Navigator initialRouteName="Users">
       <Stack.Screen
         name="Users"
         component={UserList}
         options={{
-          headerRight: () => (
-            <FormButton
-              title="Logout"
-              color="skyblue"
-              backgroundColor="#fff"
-              onPress={logout}
-            />
-          ),
+          headerRight: () => <Button title="Logout" onPress={logout} />,
         }}
       />
     </Stack.Navigator>
