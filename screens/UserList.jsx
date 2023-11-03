a29865857d2a6e8b34edef60145cb83a4e85ba83
Asad.Digimark <asad.digimark@gmail.com>
2023-11-03 15:08:22 +0500

    search box

--- screens/UserList.jsx
+++ screens/UserList.jsx
@@ -1,25 +1,32 @@
 import {useEffect, useState} from 'react';
-import {FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
 import User from '../components/User';
+import FormInput from '../components/FormInput';
+import {URL} from '../config';
 
-export default ({navigation}) => {
+export default ({navigation: {navigate}}) => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.github.com/users')
+    fetch(`${URL}/users`)
       .then(res => res.json())
       .then(data => setUsers(data));
   }, []);
 
   return users.length ? (
-    <FlatList
-      data={users}
-      renderItem={({item}) => (
-        <User
-          user={item}
-          onPress={() => navigation.navigate('Profile', item)}
-        />
-      )}
-    />
+    <View
+      style={{
+        padding: 8,
+        borderWidth: 1,
+        borderColor: 'red',
+      }}>
+      <FormInput placeholder="Search" icon="search1" backgroundColor="#fff" />
+      <FlatList
+        data={users}
+        renderItem={({item}) => (
+          <User user={item} onPress={() => navigate('Profile', item)} />
+        )}
+      />
+    </View>
   ) : null;
 };
