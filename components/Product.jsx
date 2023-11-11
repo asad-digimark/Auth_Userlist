380a29a1b4b2726e023d223d2807d3a0744ba84d
Asad.Digimark <asad.digimark@gmail.com>
2023-11-11 14:24:10 +0500

    delete with checkbox

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,27 @@
+import CheckBox from '@react-native-community/checkbox';
 import {Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
-export default ({product, onPress}) => {
+export default ({product, onPress, selection, setSelection}) => {
+  const handlePress = () => (selection.length ? toggleSelect() : onPress());
+
+  const toggleSelect = () => {
+    if (isChecked) {
+      setSelection(prev => prev.filter(p => p !== product.id));
+    } else {
+      setSelection(prev => [...prev, product.id]);
+    }
+  };
+
+  const isChecked = Boolean(selection.find(p => p === product.id));
+
   return (
-    <TouchableOpacity style={styles.product} onPress={onPress}>
+    <TouchableOpacity
+      style={styles.product}
+      onPress={handlePress}
+      onLongPress={() => setSelection([...selection, product.id])}>
+      {Object.keys(selection).length > 0 && (
+        <CheckBox value={isChecked} onValueChange={toggleSelect} />
+      )}
       <Image source={{uri: product.image}} style={styles.image} />
       <Text style={styles.td}>{product.name}</Text>
       <Text style={styles.td}>{product.brand}</Text>
--- screens/Products.jsx
+++ screens/Products.jsx
@@ -4,8 +4,8 @@ import {
   FlatList,
   StyleSheet,
   ActivityIndicator,
-  TextInput,
-  Text,
+  Alert,
+  Button,
 } from 'react-native';
 import Product from '../components/Product';
 import firestore from '@react-native-firebase/firestore';
@@ -17,6 +17,7 @@ export default ({navigation}) => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selection, setSelection] = useState([]);
 
   useEffect(() => {
     const subscriber = firestore()
@@ -64,16 +65,62 @@ export default ({navigation}) => {
       <FlatList
         data={filteredProducts}
         keyExtractor={item => item.id}
-        renderItem={({item}) => (
+        renderItem={({item, index}) => (
           <Product
             product={item}
             onPress={() => navigation.navigate('Details', item)}
+            selection={selection}
+            setSelection={setSelection}
+            index={index}
           />
         )}
         ListEmptyComponent={
           <EmptyList onPress={() => navigation.replace('Add Product')} />
         }
       />
+      {selection.length > 0 && (
+        <Footer products={selection} setSelection={setSelection} />
+      )}
+    </View>
+  );
+};
+
+const Footer = ({products, setSelection}) => {
+  const handleDelete = async () => {
+    Alert.alert('Confirm Deletion', 'Are you sure to delete?', [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {onPress: deleteProduct, text: 'Delete', style: 'destructive'},
+    ]);
+  };
+
+  const deleteProduct = async () => {
+    const batch = firestore().batch();
+
+    products.forEach(id => {
+      const docRef = firestore().collection('Products').doc(id);
+      batch.delete(docRef);
+    });
+
+    try {
+      setSelection([]);
+      const res = await batch.commit();
+      Alert.alert(undefined, 'Products deleted successfully');
+    } catch (error) {
+      console.error('Error deleting items:', error);
+    }
+  };
+
+  return (
+    <View style={styles.footer}>
+      <Button
+        title={`Delete ${products.length}`}
+        color="red"
+        onPress={handleDelete}
+      />
+      <Button title="Cancel" onPress={() => setSelection([])} />
     </View>
   );
 };
@@ -82,6 +129,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding,
+    gap: 8,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     gap: 8,
   },
 });
