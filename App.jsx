03fe58be988b085d6bfc0fb743ede935488a37d6
Asad.Digimark <asad.digimark@gmail.com>
2023-11-14 00:10:29 +0500

    implement splash screen with wonder woman

--- App.jsx
+++ App.jsx
@@ -5,8 +5,13 @@
  * @format
  */
 
+import {useEffect} from 'react';
 import AuthProvider from './auth/AuthProvider';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <AuthProvider />;
 }
